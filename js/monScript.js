df81a56b971e277c2e10a33561c88dd0fc6be58f
GabryelDupont <35579507+GabryelDupont@users.noreply.github.com>
2018-05-22 15:20:51 -0400

    Add files via upload

---
+++ js/monScript.js
@@ -0,0 +1,65 @@
+$('document').ready(function () {
+    $('#zoom1').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom2').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom3').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom4').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom5').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom6').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom7').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom8').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+    $('#zoom9').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.2 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+});
